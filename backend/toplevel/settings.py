010f8b12fb3adbcde816ca777c97e15a1bf4e3a4
pharick <artemforkunov@gmail.com>
2019-08-27 23:28:58 +0300

    image upload and number field in participant model

--- backend/toplevel/settings.py
+++ backend/toplevel/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
