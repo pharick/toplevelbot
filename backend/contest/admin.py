5657e6e9429ae210993ef0ed3f8ce58d60a99ac7
pharick <artemforkunov@gmail.com>
2019-09-15 11:39:47 +0300

    ratings in admin site

--- backend/contest/admin.py
+++ backend/contest/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Judge, Participant
+from .models import Judge, Participant, Rating
 
 admin.site.register(Judge)
 admin.site.register(Participant)
+admin.site.register(Rating)
